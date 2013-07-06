674b428e22d62428d8683d898ad8dd054b445e63
John <vagh27@gmail.com>
2013-07-06 13:40:52 -0400

    sorting by day logic

--- gitivity.js
+++ gitivity.js
@@ -28,22 +28,36 @@ Gitivity.Views.Activities = Backbone.View.extend({
         this.collection.bind("add", this.addOne, this);
     },
     render: function () {
-        console.log("render")
-        console.log(this.collection.length);
-        $(this.el).html(this.template());
+        //console.log("render")
+        //console.log(this.collection.length);
         this.addAll();
     },
     addAll: function () {
         //console.log(this.collection)
         var allData = this.collection.toJSON();
         //this.collection.each(this.addOne);
+
+        //get first day
+        var currentDayofSet = new Date(allData[0].created_at);
+        currentDayofSet = currentDayofSet.getDate();
+        $(this.el).html(this.template({ currentDayofMonth:currentDayofSet }));
+
         for(var i=0; i < allData.length; i++){
             var currentData = allData[i],
                 dateParse = new Date(allData[i].created_at),
                 currentDayofMonth = dateParse.getDate(),
                 currentDayofWeek = dateParse.getDay(),
                 currentMonth = dateParse.getMonth(),
-                url = null;
+                url = null,
+                startNewDay = false;
+
+                //alert(currentDayofSet + " "+ currentDayofMonth)
+
+                if(currentDayofSet == currentDayofMonth){  }
+                else { $(this.el).append(this.template({ currentDayofMonth:currentDayofMonth })); }
+
+                currentDayofSet = currentDayofMonth;
+
 
             if(currentData.type == 'PushEvent'){
                 url = "https://github.com/"+currentData.repo.name+"/commit/"+currentData.payload.head;
@@ -51,12 +65,13 @@ Gitivity.Views.Activities = Backbone.View.extend({
 
             //so we're passing a bunch of stuff, including currentData as a model, just so we have it
             //all this 'stuff' is pretty much to avoid having to add lots of 'if' statements in the template
-            this.addOne(currentData,currentDayofWeek,currentDayofMonth,url)
+            this.addOne(currentData,currentMonth,currentDayofMonth,url,startNewDay)
         }
     },
-    addOne: function (activity,currentDayofWeek,currentDayofMonth,url) {
-        view = new Gitivity.Views.Activity({ model: activity, currentDayofWeek:currentDayofWeek, currentDayofMonth:currentDayofMonth, url:url });
-        $("ul", this.el).append(view.render());
+    addOne: function (activity,currentMonth,currentDayofMonth,url,startNewDay) {
+        view = new Gitivity.Views.Activity({ model: activity, currentMonth:currentMonth, currentDayofMonth:currentDayofMonth, url:url, startNewDay:startNewDay });
+        
+        $("ul.d"+currentDayofMonth, this.el).append(view.render());
     }
 
 })
@@ -70,7 +85,7 @@ Gitivity.Views.Activity = Backbone.View.extend({
         //this.model.bind('destroy', this.destroyItem, this);
     },
     render: function () {
-        console.log(this.options)
+        //console.log(this.options)
         return $(this.el).append(this.template({model:this.model, extra:this.options})) ;
     }
 })
